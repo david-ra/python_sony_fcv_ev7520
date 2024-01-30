52c37a58611ddbe28d52556617572956951ad3e2
david-ra <davidrodriguez.alb@gmail.com>
2024-01-30 18:55:36 -0300

    redis config

--- scripts/read_stream_from_redis.py
+++ scripts/read_stream_from_redis.py
@@ -10,16 +10,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 redis_cli = redis.StrictRedis(
-    host=os.environ["REDIS_REPLICA_2_HOST"],
-    port=int(os.environ["REDIS_REPLICA_2_PORT"]),
-    password=os.environ["REDIS_REPLICA_2_PASSWORD"])
+    host=os.environ["REDIS_HOST"],
+    port=int(os.environ["REDIS_PORT"]),
+    password=os.environ["REDIS_PASSWORD"])
 
 #numpy_bytes = redis_cli.get("camera_0")
 #decoded = np.frombuffer(numpy_bytes, dtype=np.uint8).reshape((720, 1280,3))
 while True:
     numpy_bytes = redis_cli.get("camera_0")
-    decoded = np.frombuffer(numpy_bytes, dtype=np.uint8).reshape((720, 1280,3))
-    cv2.imshow('frame', decoded)
+    if numpy_bytes:
+        decoded = np.frombuffer(numpy_bytes, dtype=np.uint8).reshape((720, 1280,3))
+        cv2.imshow('frame', decoded)
 
     key = cv2.waitKey(3)
     if key == 27:
